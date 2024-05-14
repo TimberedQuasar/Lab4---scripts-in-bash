bebfacec1b858ba4695a5ccf5d28956b437ce9a8
Szymon <dsw54395@student.dsw.edu.pl>
2024-05-14 13:22:49 +0200

    script for a --logs parameter

--- skrypt.sh
+++ skrypt.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-today=$(date +"%d-%m-%Y")
-date -d "Today's date: $today"
+#show date --date
+#today=$(date +"%d-%m-%Y")
+#date -d "Today's date: $today"
+
+#create 100 files named "logx.txt"--logs
+for file_number in {1..100}; do
+  file_name="log${file_number}.txt"
+  echo "$file_name created by $(basename "$0")" > "$file_name"
+done
