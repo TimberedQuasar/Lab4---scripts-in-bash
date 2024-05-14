11de729f198e42b94ea2e48773ad1cac0a5f2288
Szymon <dsw54395@student.dsw.edu.pl>
2024-05-14 13:31:53 +0200

    script for a --help parameter

--- skrypt.sh
+++ skrypt.sh
@@ -5,7 +5,13 @@
 #date -d "Today's date: $today"
 
 #create 100 files named "logx.txt"--logs
-for file_number in {1..100}; do
-  file_name="log${file_number}.txt"
-  echo "$file_name created by $(basename "$0")" > "$file_name"
-done
+#for file_number in {1..100}; do
+#  file_name="log${file_number}.txt"
+#  echo "$file_name created by $(basename "$0")" > "$file_name"
+#done
+
+#--help
+echo "--date will show you today's date"
+echo "--logs wil create 100 files named "logx.txt" where x=number"
+echo "--logs with argument like:30 will create 30 files"
+echo "--help for showing available options"
